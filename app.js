4b74266930940e530b6c8301de8f6b7fd9a9d320
Priyam Bhattacharya <priyambhattacharya6@gmail.com>
2022-02-26 09:19:04 +0530

    removeds custom css classes and added overflow-y-hidden in body

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ let nav_item = document.getElementById('nav-items');
 let ham_menu_close = document.querySelector('#ham-menu-close');
 let ham_menu = document.querySelector('#ham-menu');
 let main = document.querySelector('#main')
+let body = document.querySelector('#body')
 
 ham.addEventListener('click' , function(e){
-    nav_item.classList.toggle('left-960');
+    body.classList.toggle('overflow-y-hidden')
+    nav_item.classList.toggle('-left-[1024px]');
     nav_item.classList.toggle('left-0');
     ham_menu.classList.toggle('hidden');
     ham_menu_close.classList.toggle('hidden');
     ham_menu_close.classList.toggle('block');
-    main.classList.toggle('pe-none')
+    main.classList.toggle('pointer-events-none')
 })
